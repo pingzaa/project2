7069695b6fabfad7128a5cb9136bc93687d038a2
sattawat Chaikum <pingza@gmail.con>
2020-08-16 13:18:29 +0700

    update

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            ["name" => "ซื้อของ"],
+            ["name" => "ออกกำลังขาย"],
+            ["name" => "ท่องเที่ยว"],
+        ];
+
+        Category::insert($data);
+    }
+}
