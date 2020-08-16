c8e8317d01d01a6d45adef2ac8559452843afbe8
sattawat Chaikum <pingza@gmail.con>
2020-08-16 15:23:18 +0700

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,32 +1,35 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Post;
 use Illuminate\Http\Request;
-
 class PostController extends Controller
 {
+    // Post
     public function index()
     {
+        $posts = Post::all();
+        $data = [
+            'posts' => $posts
+        ];
+        return view('post.index', $data);
     }
-
-    public function create()
+    public function show($id)
     {
+        $category = Post::find($id);
+        return $category;
     }
 
     public function store(Request $request)
     {
     }
-
     public function edit($id)
     {
     }
-
     public function update(Request $request)
     {
     }
-
     public function delete($id)
     {
     }
 }
+.
